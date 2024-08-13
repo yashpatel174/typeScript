ef1b656dbaa9725b3acfec4bfb40612fd37e1c69
Yash Patel <yash.patel@vindaloosofttech.com>
2024-08-13 19:17:56 +0530

    typeScript

---
+++ interface.ts
@@ -0,0 +1,34 @@
+// interface User {
+//   readonly dbId: number;
+//   email: string;
+//   userId: number;
+//   googleId?: string;
+//   // startTrial: () => string;
+//   startTrial(): string;
+//   getCoupon(couponName: string, value: number): number;
+// }
+
+// interface User {
+//   githubToken: string;
+// }
+
+// interface Admin extends User {
+//   role: "admin" | "group" | "user";
+// }
+
+// const yash: Admin = {
+//   dbId: 1,
+//   role: "admin",
+//   email: "Yash@gmail.com",
+//   userId: 2210,
+//   githubToken: "github",
+//   startTrial: () => {
+//     return "trial started";
+//   },
+//   getCoupon: (name: "yash10", off: 10) => {
+//     return 10;
+//   },
+// };
+
+// yash.email = "y@gmail.com";
+// // yash.dbId = 1
