a04c7d50b9e40dbe699997dee7ef742d97a0b109
Yash Patel <yash.patel@vindaloosofttech.com>
2024-08-14 12:41:16 +0530

    final update

--- pureTS/src/TypeNarrow.ts
+++ pureTS/src/TypeNarrow.ts
@@ -41,3 +41,27 @@ function isAdminAccount(account: User | Admin) {
     return account.isAdmin;
   }
 }
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "Fish food";
+  } else {
+    pet;
+    return "Bird food";
+  }
+}
---
+++ pureTS/src/discriminatedUnion.ts
@@ -0,0 +1,38 @@
+// interface Circle {
+//   kind: "circle";
+//   radius: number;
+// }
+
+// interface Square {
+//   kind: "square";
+//   side: number;
+// }
+
+// interface Rectangle {
+//   kind: "rectangle";
+//   length: number;
+//   width: number;
+// }
+
+// type Shape = Circle | Square | Rectangle;
+
+// function getTrueShape(shape: Shape) {
+//   if (shape.kind === "circle") {
+//     return Math.PI * shape.radius ** 2;
+//   }
+//   //   return shape.side * shape.side;
+// }
+
+// function getArea(shape: Shape) {
+//   switch (shape.kind) {
+//     case "circle":
+//       return Math.PI * shape.radius ** 2;
+
+//     case "Square":
+//       return shape.side * shape.side;
+
+//     default:
+//       const defaultShape: never = shape;
+//       return defaultShape;
+//   }
+// }
